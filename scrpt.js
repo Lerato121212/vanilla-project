f81da8d92ef12e41641353127a7deb3a7a410843
Lerato Sejake <Lerato121212@users.noreply.github.com>
2024-04-09 11:54:35 +0200

    add more functions

--- scrpt.js
+++ scrpt.js
@@ -1,34 +1,47 @@
 function weatherResponse(response) {
-let temperatureElement =document.querySelector("#temperature")
-let temperature = reponse.data.temperature.current;
-let cityElement=document.querySelector("#city");
-let descriptionElement =document.querySelector("#description");
-let humidityElement = document.querySelector("#humidity");
-let windspeedElement = document.querySelector("#wind-speed");
-let timeElement = document.querySelector("#time");
-let date = new date("response.data.time*1000");
-let iconElement = document.querySelector("#icon");
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = reponse.data.temperature.current;
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windspeedElement = document.querySelector("#wind-speed");
+  let timeElement = document.querySelector("#time");
+  let date = new date("response.data.time*1000");
+  let iconElement = document.querySelector("#icon");
 
-cityElement.innerHTML=response.data.city;
-timeElement.innerHTML=formatDate(date);
-descriptionElement.innerHTML=response.data.condition.description;
-humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
-windspeedElement.innerHTML = `${response.data.wind.speed}km/h`;
-temperatureElement.innerHTML = Math.round(temperature);
-iconElement.innerhtml = `<img src="${response.data.condition.icon_url}" class="weather-app-icon" />`
+  cityElement.innerHTML = response.data.city;
+  timeElement.innerHTML = formatDate(date);
+  descriptionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  windspeedElement.innerHTML = `${response.data.wind.speed}km/h`;
+  temperatureElement.innerHTML = Math.round(temperature);
+  iconElement.innerhtml = `<img src="${response.data.condition.icon_url}" class="weather-app-icon" />`;
 }
 function formatDate(date) {
   let minutes = date.getMinutes();
   let hours = date.getHours();
-  let days = ["Sunday",
-"Monday",
-"Tuesday",
-"Wednesday",
-"Thursday",
-"Friday",
-"Saturday",];
-let day = days[date.getDay()];
-if (minutes < 10) {
-  minutes = `0${minutes}`;
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  return `${day} ${hours} ${minutes}`;
 }
+function submitSearchForm(event) {
+  event.preventDefault();
+  let inputSearchButton = document.querySelector("#inputSearchButton");
+
+  searchFormCity(inputSearchButton.value);
 }
+
+let formElement = document.querySelector("#form");
+formElement.addEventListener("submit", submitSearchForm);
+searchFormCity("Paris");
