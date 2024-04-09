9281be960f15134ffa7b2945d43c8b161b8aae33
Lerato Sejake <Lerato121212@users.noreply.github.com>
2024-04-09 10:59:15 +0200

    eddit the js

--- scrpt.js
+++ scrpt.js
@@ -8,16 +8,12 @@ let windspeedElement = document.querySelector("#wind-speed");
 let timeElement = document.querySelector("#time");
 let date = new date("response.data.time*1000");
 let iconElement = document.querySelector("#icon");
-
+}
 cityElement.innerHTML=response.data.city;
 timeElement.innerHTML=formatDate(date);
 descriptionElement.innerHTML=response.data.condition.description;
 humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
 windspeedElement.innerHTML = `${response.data.wind.speed}km/h`;
 temperatureElement.innerHTML = Math.round(temperature);
-iconElement.innerhtml = `<img src="${response.data.condition.icon_url}" class="weather-app-icon" />`;`;
-}
+iconElement.innerhtml = `<img src="${response.data.condition.icon_url}" class="weather-app-icon" />`
 
-function formatDate(date){
-  
-}
